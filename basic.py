f27a98c91a2fb06a3c4c7c93926de6c3ae240a00
Mehvix <maxlangdonvogel@gmail.com>
2018-09-12 21:47:00 -0500

    Added .about

--- basic.py
+++ basic.py
@@ -21,6 +21,20 @@ class Basic:
     print("Loading Basic...")
 
     @client.command()
+    async def about(self, ctx):
+        info = await self.client.application_info()
+        embed = discord.Embed(
+            title="Info about {}:".format(info.name),
+            description="[Github](https://github.com/Mehvix/synapsBotRW)",
+            color=settings.embed_color)
+        embed.add_field(name="Creator:", value=info.owner, inline=True)
+        embed.add_field(name="Python Version:", value=sys.version.split()[0], inline=True)
+        embed.add_field(name="Discord.py Version:", value=discord.__version__, inline=True)
+        embed.add_field(name="Client Version:", value=settings.get_version(), inline=True)
+        embed.set_thumbnail(url=self.client.user.avatar_url)
+        await ctx.send(embed=embed)
+
+    @client.command()
     async def ping(self, ctx):
         await ctx.send("I have a latency of `{}` ms".format(str(float(self.client.latency)*1000)[:2]))
 
