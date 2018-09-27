c8b314056774b5d468c14562b03640d0f5e7e558
Mehvix <maxlangdonvogel@gmail.com>
2018-09-26 21:06:26 -0500

    added notation roles to .accept

--- accept.py
+++ accept.py
@@ -28,8 +28,7 @@ class Accept:
 
     @commands.command()
     async def accept(self, ctx):
-        member_role_name = settings.member_role_name
-        member_role = discord.utils.get(ctx.guild.roles, name=member_role_name)
+        member_role = discord.utils.get(ctx.guild.roles, name=settings.member_role_name)
         if member_role is None:
             try:
                 member_role = await ctx.guild.create_role(name=member_role_name, color=discord.Color(0x939393))
@@ -39,27 +38,29 @@ class Accept:
         if member_role in ctx.author.roles:
             return  # User already has the role
         else:
-            try:
-                await ctx.message.author.add_roles(member_role, reason="User typed `.accept` in {}".format(ctx.channel))
-                channel = self.client.get_channel(id=settings.notification_channel)
-                await channel.send("<@{}> typed `.accept` :ok_hand:".format(ctx.author.id))
-
-            except discord.Forbidden:
-                await ctx.guild.owner.send(
-                    "I tried to add role `{}` to `{}` in `{}` but don't have the permission to!".format(member_role_name, ctx.author.name, ctx.guild.name))
+            await ctx.message.author.add_roles(member_role, reason="User typed `.accept` in {}".format(ctx.channel))
+            channel = self.client.get_channel(id=settings.notification_channel)
+            await channel.send("<@{}> typed `.accept` :ok_hand:".format(ctx.author.id))
 
     @client.event
     async def on_message(self, message):
         if message.channel.id == settings.accept_channel and message.author.id != self.client.user.id:
+            level_role = discord.utils.get(ctx.guild.roles, id=settings.level_role)
+            if level_role and level_role not in member.roles:
+                roles_role = discord.utils.get(ctx.guild.roles, id=settings.roles_role)
+                groups_role = discord.utils.get(ctx.guild.roles, id=settings.groups_role)
+                games_role = discord.utils.get(ctx.guild.roles, id=settings.games_role)
+                restriction_role = discord.utils.get(ctx.guild.roles, id=settings.restriction_role)
+                level1_role = discord.utils.get(ctx.guild.roles, name="Level 1")
+
+                await member.add_roles(level_role, roles_role, groups_role, games_role, restriction_role, level1_role,
+                                       reason="Added notation roles")
             await asyncio.sleep(1)
+
             try:
                 await message.delete()
-            except discord.Permissions:
-                await ctx.guild.owner.send(
-                    "I tried to clear a message in {}, but I don't have the permission to!".format(message.channel.name))
             except discord.NotFound:
-                pass  # message already gone
-
+                pass  # message already gone (deleted)
 
 
 def setup(client):
