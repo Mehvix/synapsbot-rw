91dbdf8a51cfa11671db7e2b81229eb234de7f7f
MehvixDesktop <mehvix@protonmail.com>
2019-03-17 20:31:18 -0500

    added TODO

--- qr.py
+++ qr.py
@@ -16,12 +16,12 @@ class QR(commands.Cog):
 
     print("Loading QR...")
 
+    # TODO Fix this
     @commands.command(aliases=["createqr", "qrcode"], usage="[data]", description="Creates an QR code", brief="Creates a QR code that links to [data]")
     async def qr(self, ctx, data):
         url = pyqrcode.create(data)
         with open('code.png', 'wb') as fstream:
             url.png(fstream, scale=3)
-
         buffer = io.BytesIO()
         url.png(buffer)
 
